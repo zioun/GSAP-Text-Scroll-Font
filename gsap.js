947b8c9aca8b6f0f68d9d1a80a78a16b7681102f
zioun <jionkhan0@gmail.com>
2025-09-05 21:03:38 +0600

    update

--- gsap.js
+++ gsap.js
@@ -1,13 +1,62 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".scroll-font", {
-  fontSize: "500px",
+let tl = gsap.timeline({
   scrollTrigger: {
     trigger: ".scroll-font-area",
     start: "top top",
-    end: "+=70%",
-    scrub: 1, // smooth follow: 1 second lag
+    end: "+=100%", // পুরো scroll duration
+    scrub: 1,
     pin: true,
   }
 });
 
+// Step 1: 
+tl.to(".scroll-font", {
+  fontSize: "150px",
+  ease: "power2.out",
+  scrub: 1,
+  duration: 5 
+});
+
+// Step 2: 
+tl.to(".scroll-font", {
+  ease: "power2.out",
+  duration: 5
+});
+// Step 3: 
+tl.to(".hidden0", {
+  display: "inline-block",
+  
+  ease: "power2.out",
+  duration: 5
+});
+tl.to(".hidden1", {
+  display: "inline-block",
+  ease: "power2.out",
+  duration: 5
+});
+tl.to(".hidden2", {
+  display: "inline-block",
+  ease: "power2.out",
+  duration: 5
+});
+tl.to(".hidden3", {
+  display: "inline-block",
+  ease: "power2.out",
+  duration: 5
+});
+tl.to(".hidden4", {
+  display: "inline-block",
+  ease: "power2.out",
+  duration: 5
+});
+tl.to(".hidden5", {
+  display: "inline-block",
+  ease: "power2.out",
+  duration: 5
+});
+tl.to(".hidden6", {
+  display: "inline-block",
+  ease: "power2.out",
+  duration: 5
+});
